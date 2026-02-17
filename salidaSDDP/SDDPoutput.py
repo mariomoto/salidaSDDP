cc3c0903b5ee800325b914d5669b13195e072180
Mario Amaro <amaro.mario@gmail.com>
2026-02-17 09:05:52 -0300

    sigue creacion de diccionario.

--- salidaSDDP/SDDPoutput.py
+++ salidaSDDP/SDDPoutput.py
@@ -4,7 +4,19 @@ study = psr.factory.load_study(
     r"C:\Users\mamaro\OneDrive - Colbun S.A\Gerencia de Planificación - General\Chile\SDDP\2025\2025-10Xrh"
     )
 thermal_plants = study.get("ThermalPlant")
-if isinstance(thermal_plants, list):
-    dict_thermal_plants = {plant.name.strip(): plant.code for plant in thermal_plants}  
-    print(dict_thermal_plants) 
+batteries = study.get("Battery")
+hydro_plants = study.get("HydroPlant")
+renewable_plants = study.get("RenewablePlant")
+dict_plants = {}
+for plant_list in [hydro_plants, thermal_plants, renewable_plants, batteries]:
+    if isinstance(plant_list, list):
+        for plant in plant_list:
+            dict_plants[plant.name.strip()] = plant.code
+print(f"dict_plants = {dict_plants}\n") 
 
+buses = study.get("Bus")
+dict_buses = {}
+if isinstance(buses, list):
+    for bus in buses:
+        dict_buses[bus.name.strip()] = bus.code
+print(f"dict_buses = {dict_buses}\n") 
